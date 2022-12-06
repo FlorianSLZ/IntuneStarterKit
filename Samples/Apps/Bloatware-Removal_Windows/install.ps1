e4d77f58eda9a522ee5c74517b56fdbff9093abb
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2022-12-06 20:54:32 +0100

    apps initial

---
+++ Samples/Apps/Bloatware-Removal_Windows/install.ps1
@@ -0,0 +1,46 @@
+﻿$PackageName = "Bloatware-Removal_Windows"
+
+$Path_local = "$Env:Programfiles\_MEM"
+Start-Transcript -Path "$Path_local\Log\$PackageName-install.log" -Force
+try{
+    $AppList = Get-Content "WindowsApps.txt"
+    Write-Host "----------------------------------------------"
+    Write-Host "APPs to REMOVE"
+    Write-Host "----------------------------------------------"
+    $AppList
+    Write-Host "----------------------------------------------"
+
+
+    ForEach ($App in $AppList) {
+        $App = $App.TrimEnd()
+        $PackageFullName = (Get-AppxPackage $App).PackageFullName
+        $ProPackageFullName = (Get-AppxProvisionedPackage -online | where {$_.Displayname -eq $App}).PackageName
+
+        if ($PackageFullName) {
+            Write-Host "Removing Package: $App"
+            Start-Sleep -Seconds 5
+            Remove-AppxPackage -package $PackageFullName
+        }
+        else {
+            "Unable to find package: $App" 
+        }
+
+        if ($ProPackageFullName) {
+            Write-Host "Removing Provisioned Package: $ProPackageFullName"
+            Start-Sleep -Seconds 5 
+            Remove-AppxProvisionedPackage -online -packagename $ProPackageFullName  
+        }
+        else {
+            "Unable to find provisioned package: $App"
+        }
+    }
+
+    # Validation file
+    New-Item -Path "$Path_local\Validation\$PackageName" -ItemType "file" -Force 
+    
+}catch{
+    Write-Error $_
+}
+
+
+Stop-Transcript
---
+++ Samples/Apps/MS Apps Business EN/install.ps1
@@ -0,0 +1,11 @@
+﻿$PackageName = "MSApps_Business_DE_x64"
+
+$Path_local = "$Env:Programfiles\_MEM"
+Start-Transcript -Path "$Path_local\Log\$PackageName-install.log" -Force
+
+$install_path = "$Path_local\Data\M365Apps"
+New-Item -Type Directory -Path $install_path -Force
+
+Start-Process "setup.exe" -Argument "/configure $PackageName.xml" -wait
+
+Stop-Transcript
